1d0cce8f731a2bf9e1ee26ecae4b58653511fbc4
Brunna99 <brunnamcamagalhaes@gmail.com>
2024-06-25 21:19:51 -0400

    Estilizei cores e formas, e adicionei JavaScript

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("form-tabuada");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const numero = document.getElementById("numero").value;
+  const resultado = document.getElementById("resultado");
+
+  const tabela = document.createElement("table");
+  tabela.classList.add("tabela");
+  const cabecalho = document.createElement("thead");
+  const tituloCabecalho = document.createElement("tr");
+  tituloCabecalho.innerText = "Resultado";
+  cabecalho.appendChild(tituloCabecalho);
+
+  const corpo = document.createElement("tbody");
+
+  tabela.appendChild(cabecalho);
+  tabela.appendChild(corpo);
+
+  resultado.appendChild(tabela);
+
+  for (let i = numero; i <= numero; i++) {
+    for (let y = 1; y <= 10; y++) {
+      const linha = document.createElement("tr");
+      linha.innerText = i + " * " + y + " = " + i * y;
+      corpo.appendChild(linha);
+    }
+  }
+});
